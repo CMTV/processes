bc2707efcde89d526d570488c6357bd7ac44ead8
CMTV <cmtvpetya@gmail.com>
2022-04-03 13:19:02 +0300

    v2.0.1

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { SyncProcess, AsyncProcess } from "src/Process";
+import { SyncProcessGroup, AsyncProcessGroup } from "src/ProcessGroup";
+
+export { SyncProcess, AsyncProcess, SyncProcessGroup, AsyncProcessGroup };
